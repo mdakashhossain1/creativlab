2d78b5b9b86889ce7337461086e2aadf1797d6e9
mdakashhossain1 <hossainakash22958@gmail.com>
2026-06-20 18:22:58 +0530

    style(ui): add btn-close-dark class to user edit modal close button

--- resources/views/admin/user/user_show.blade.php
+++ resources/views/admin/user/user_show.blade.php
@@ -332,7 +332,7 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="exampleModalLabel">{{ __('Edit User Basic Information') }}</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    <button type="button" class="btn-close btn-close-dark" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
                     <form action="{{ route('admin.user-update', $user->id) }}" method="POST">
