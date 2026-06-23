8c9c9e7f874f8ea56f81899670f6f889a1de3892
mdakashhossain1 <hossainakash22958@gmail.com>
2026-06-23 22:26:47 +0530

    feat: scaffold Payroll module structure

---
+++ Modules/Payroll/App/Providers/RouteServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Modules\Payroll\App\Providers;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+
+class RouteServiceProvider extends ServiceProvider
+{
+    protected string $moduleNamespace = 'Modules\Payroll\App\Http\Controllers';
+
+    public function boot(): void { parent::boot(); }
+
+    public function map(): void
+    {
+        $this->mapApiRoutes();
+        $this->mapWebRoutes();
+    }
+
+    protected function mapWebRoutes(): void
+    {
+        Route::middleware('web')
+            ->namespace($this->moduleNamespace)
+            ->group(module_path('Payroll', '/routes/web.php'));
+    }
+
+    protected function mapApiRoutes(): void
+    {
+        Route::prefix('api')
+            ->middleware('api')
+            ->namespace($this->moduleNamespace)
+            ->group(module_path('Payroll', '/routes/api.php'));
+    }
+}
