bdeb2175c9ddd78250b6823dc4c31d333a2e2207
mdakashhossain1 <hossainakash22958@gmail.com>
2026-06-24 17:27:25 +0530

    feat: add configuration file for ArknoxMonitor module

--- Modules/ArknoxMonitor/config/config.php
+++ Modules/ArknoxMonitor/config/config.php
@@ -3,7 +3,7 @@
 return [
     'route_prefix'        => 'arknox-monitor',
     'base_rent'           => 7.00,
-    'free_queries'        => 10000,
-    'overage_rate'        => 0.0001,
+    'free_queries'        => 0,
+    'overage_rate'        => 0.001,
     'exclude_connections' => [],
 ];
